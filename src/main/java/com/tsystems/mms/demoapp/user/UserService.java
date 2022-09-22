48e512ccd08fdb544608c919a1cb0de3126e30b2
laszlo hujber <l.hujber@t-systems.com>
2022-09-22 08:40:15 +0200

    Add find user by id

--- src/main/java/com/tsystems/mms/demoapp/user/UserService.java
+++ src/main/java/com/tsystems/mms/demoapp/user/UserService.java
@@ -1,8 +1,9 @@
 package com.tsystems.mms.demoapp.user;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * This service manages all user.
@@ -11,17 +12,30 @@ import java.util.List;
 @Transactional
 public class UserService {
 
-  private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-  public UserService(UserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-  /**
-   * Find all users from the database.
-   * @return List of users.
-   */
-  public List<User> getAll() {
-    return userRepository.findAll();
-  }
+	/**
+	 * Find all users from the database.
+	 * 
+	 * @return List of users.
+	 */
+	public List<User> getAll() {
+		return userRepository.findAll();
+	}
+
+	/**
+	 * Find user from the database by id.
+	 * 
+	 * @param The user id.
+	 * @throws UserNotFoundException if the user is not found with given id.
+	 * @return One user.
+	 */
+	public User getUserById(Long userId) {
+		return userRepository.findById(userId)
+				.orElseThrow(() -> new UserNotFoundException("User not found with id:" + userId));
+	}
 }
