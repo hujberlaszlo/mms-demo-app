c632bcf59a7bf22a28fd938c404645f1973cffa4
laszlo hujber <l.hujber@t-systems.com>
2022-09-22 09:03:30 +0200

    Add surName and firstName to User entity

--- src/main/java/com/tsystems/mms/demoapp/user/User.java
+++ src/main/java/com/tsystems/mms/demoapp/user/User.java
@@ -12,28 +12,51 @@ import java.io.Serializable;
 @Table(name = "demo_user")
 public class User implements Serializable {
 
-  private static final long serialVersionUID = 1715994813284718249L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", nullable = false)
-  protected Long id;
+	private static final long serialVersionUID = 1715994813284718249L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	protected Long id;
 
-  @Column(name = "email", nullable = false)
-  private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-  public Long getId() {
-    return id;
-  }
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	@Column(name = "sur_name", nullable = false)
+	private String surName;
 
-  public String getEmail() {
-    return email;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
 }
